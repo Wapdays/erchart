0f38d8e1165460ed8b134ebf84395652901180c5
Wapdays <wapdays@protonmail.com>
2019-06-23 13:39:13 +0100

    Updates

---
+++ precache-manifest.cd581617c9b1cf8c1fed9977158fdaa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de3889d9ddc7f951e9f6b8c99c36ac8d",
+    "url": "/erchart/index.html"
+  },
+  {
+    "revision": "0ed3c0b1b46121c1f5e8",
+    "url": "/erchart/static/css/main.a7d3cebb.chunk.css"
+  },
+  {
+    "revision": "5ba135fafe3c0d1556fb",
+    "url": "/erchart/static/js/2.991fa584.chunk.js"
+  },
+  {
+    "revision": "0ed3c0b1b46121c1f5e8",
+    "url": "/erchart/static/js/main.1d347be5.chunk.js"
+  },
+  {
+    "revision": "9339096f2a26f41d944c",
+    "url": "/erchart/static/js/runtime~main.3154189b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/erchart/static/media/logo.5d5d9eef.svg"
+  }
+]);
